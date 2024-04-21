890352670a42f616118be95bd620052d4884f981
davidho27941 <davidho.prime@gmail.com>
2024-04-21 08:36:20 +0000

    refactor: move two condition (single/multiple) to individual modules.

--- LightningTS/data/io.py
+++ LightningTS/data/io.py
@@ -5,52 +5,62 @@ from glob import glob
 
 from .base import BaseDataIO
 
+
 class CSVDataIO(BaseDataIO):
     def __init__(self, file_path, **kwargs):
         super().__init__(self, file_path)
-        
-        self.file_path = file_path
+
         self.kwargs = kwargs
-        
+
+    def read_single(self):
+        self.data = pd.read_csv(self.file_path, self.kwargs)
+
+    def read_multiple(self):
+        files = sorted(glob(f"{self.file_path}/*.csv"))
+        self.data = pd.DataFrame()
+        for file in files:
+            self.data = pd.concat(
+                [self.data, pd.read_csv(file, self.kwargs)],
+                ignore_index=True,
+            )
+
     def read(self):
-        
+
         if os.path.isdir:
-            files = sorted(glob(f"{self.file_path}/*.csv"))
-            self.data = pd.DataFrame()
-            for file in files:
-                self.data = pd.concat(
-                    [self.data, pd.read_csv(file, self.kwargs)],
-                    ignore_index=True,
-                )
-            
+            self.read_multiple()
         else:
-            self.data = pd.read_csv(self.file_path, self.kwargs)
-       
-    @property 
+            self.read_single()
+
+    @property
     def data(self):
         return self.data
-    
+
+
 class ParquetDataIO(BaseDataIO):
     def __init__(self, file_path, **kwargs):
-        super().__init__(self, file_path)
-        
-        self.file_path = file_path
+        super(ParquetDataIO, self).__init__(file_path)
+
         self.kwargs = kwargs
-        
+
+    def read_single(self):
+        self.data = pd.read_parquet(self.file_path, self.kwargs)
+
+    def read_multiple(self):
+        files = sorted(glob(f"{self.file_path}/*.csv"))
+        self.data = pd.DataFrame()
+        for file in files:
+            self.data = pd.concat(
+                [self.data, pd.read_parquet(file, self.kwargs)],
+                ignore_index=True,
+            )
+
     def read(self):
-        
+
         if os.path.isdir:
-            files = sorted(glob(f"{self.file_path}/*.parquet"))
-            self.data = pd.DataFrame()
-            for file in files:
-                self.data = pd.concat(
-                    [self.data, pd.read_parquet(file, self.kwargs)],
-                    ignore_index=True,
-                )
-            
+            self.read_multiple()
         else:
-            self.data = pd.read_parquet(self.file_path, self.kwargs)
-       
-    @property 
+            self.read_single()
+
+    @property
     def data(self):
         return self.data
