82eadcafb79fecb9681061d167c6e021ad7d0a03
davidho27941 <davidho.prime@gmail.com>
2024-05-04 03:30:09 +0000

    chore: move config utils to utils folder.

--- LightningTS/data/io.py
+++ LightningTS/data/io.py
@@ -69,21 +69,4 @@ class ParquetDataIO(BaseDataIO):
         return self.data
 
 
-class ConfigUtils:
 
-    @staticmethod
-    def load_config(config_dir: str) -> dict[str, Any]:
-        try:
-            with open(config_dir, "r") as file:
-                config = yaml.load(file, Loader=yaml.FullLoader)
-        except Exception as e:
-            raise e
-        return config
-
-    @staticmethod
-    def save_config(config, output_dir: str) -> None:
-        try:
-            with open(output_dir, "w") as file:
-                yaml.dump(config, file)
-        except Exception as e:
-            raise e
